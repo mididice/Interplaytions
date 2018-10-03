6703821d196c01cd347afe397cce47b1bf9ef0ed
Kim TeaHwan <kth004@naver.com>
2018-10-03 17:36:26 +0900

    MoeErroeModify

--- Assets/EmptyCrash.cs
+++ Assets/EmptyCrash.cs
@@ -6,6 +6,7 @@ public class EmptyCrash : MonoBehaviour {
 
 	private SpriteRenderer myRenderer;
 	public Sprite []sprite;
+	public GameObject curPlayer;
 	// Use this for initialization
 	void Start () {
 		if (myRenderer == null) {
@@ -16,16 +17,29 @@ public class EmptyCrash : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
 			int curType=GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getGetType();
+			int BackPosChk = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk ();
 			if (myRenderer == null) {
 				myRenderer = GetComponent<SpriteRenderer> ();
 				int pickType = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getPos ();
-				if (curType > 0)
-					myRenderer.sprite = sprite [0];
+				if (curType > 0) {
+					if (BackPosChk==1) {
+						GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().curPosPush (curType);
+						myRenderer.sprite = sprite [0];
+					} 
+				}
 			} else {
-				if (curType > 0)
-					myRenderer.sprite = sprite [0];
+				if (curType > 0) {
+					if (BackPosChk==1) {
+						GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().curPosPush (curType);
+						myRenderer.sprite = sprite [0];
+					}
+				}
 			}
 		}
+	}
+
+	public void Setrenderer(){
+		myRenderer.sprite = sprite [1];
 	}
 	
 	// Update is called once per frame
---
+++ Assets/StartBackground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBackground : MonoBehaviour {
+
+	public GameObject BgCube;
+	private int[,] Matrix;
+	private int row, col;
+	// Use this for initialization
+	void Start () {
+		row = 15;
+		col = 7;
+
+		Matrix= new int[row, col];
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				Matrix [i, j] = 0;
+			}
+		}
+
+		float cubeRow =BgCube.transform.localScale.x+0.1f;
+		float cubeCol = BgCube.transform.localScale.y+0.1f;
+		Vector3 initPosition = new Vector3 (-9.7f, -4.17f, 0);
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				int idx = Matrix [i, j];
+				Instantiate (BgCube, new Vector3 (initPosition.x + (cubeRow * i), initPosition.y + (cubeCol * j), initPosition.z), Quaternion.identity);
+			}
+		}
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
