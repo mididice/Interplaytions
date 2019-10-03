488df4d42845cbaa04aee6057b7b4f1420f59c45
ho9science <full17@naver.com>
2019-10-03 11:09:14 +0900

    show result tile

--- Assets/MatrixCreate.cs
+++ Assets/MatrixCreate.cs
@@ -10,9 +10,11 @@ public class MatrixCreate : MonoBehaviour {
 	public GameObject player;
 	public GameObject []cube;
 	public Sprite[] BlackSprite;
+    public Sprite[] resultSprite;
 	private GameObject[,] matrixCube;
 	private GameObject curPlayer;
-	// Use this for initialization
+    // Use this for initialization
+    private int[] typeArray;
 	private int xPos,yPos,row, col,getType,curType;
 	private int[,] Matrix;
 	private int[,] visit;
@@ -86,6 +88,7 @@ public class MatrixCreate : MonoBehaviour {
         Time.timeScale = 1;
         PlayerPrefs.SetString ("UserMatrix", "");
 		PlayerPrefs.SetInt ("Score", 0);
+        PlayerPrefs.SetString("resultTiles", "");
 		Contectedmatrix = "";
 		gameOverPanel.SetActive (false);
 		gameWinnerPanel.SetActive (false);
@@ -94,12 +97,12 @@ public class MatrixCreate : MonoBehaviour {
 		getType = -1;
 		curType = -1;
 		bottomTileIdx = 1;
+        typeArray = new int[6];
 		completeCube = new bool[6];
 		Matrix= new int[row, col];
 		visit = new int[row, col];
 		matrixCube = new GameObject[row,col];
-        
-        
+
         for (int i = 0; i < 6; i++) completeCube[i] = false;
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
@@ -139,8 +142,9 @@ public class MatrixCreate : MonoBehaviour {
     public void PlayerEnd(int status){
        
 		curPlayer.SetActive (false);
-		if (status == 0)
+        if (status == 0) {
 			gameOverPanel.SetActive (true);
+        }
 		else
 			gameWinnerPanel.SetActive (true);
       //  string file=GameObject.Find("MidiFileChecking").GetComponent<midPlayer>().GetCombineFile();
@@ -150,6 +154,7 @@ public class MatrixCreate : MonoBehaviour {
 
     private void EndSceneCall()
     {
+        Debug.Log("End Scene Call");
         string file = GameObject.Find("MidiFileChecking").GetComponent<midPlayer>().GetCombineFile();
         GameObject.Find("MidiFileChecking").GetComponent<testmid>().SetsFile(file);
         SceneManager.LoadScene("endscene");
@@ -206,16 +211,18 @@ public class MatrixCreate : MonoBehaviour {
 	public int getPos(int x,int y){
 		return Matrix [x, y];
 	}
-    
-    public bool chkCompleteCube(int idx)
-    {
+    public int[] getTypeArray() {
+        return typeArray;
+    }
+    public bool chkCompleteCube(int idx) {
         return completeCube[idx];
     }
 	public void setGetType(int val){ // set current Pick ColorType status. 
 		if (val == -1) {
 			if (stack.Count > 1) {
 				string typeName = "BottomTile" + bottomTileIdx.ToString ();
-				GameObject.Find(typeName).GetComponent<Image>().sprite=BlackSprite [getType-1];
+                typeArray[bottomTileIdx] = getType - 1;
+                GameObject.Find(typeName).GetComponent<Image>().sprite=BlackSprite [getType-1];
 				bottomTileIdx++;
 				GameObject.Find ("FontController").GetComponent<FontController> ().CountingScore ((stack.Count - 1) * 100 + 1000);
                 GameObject.Find("MidiFileChecking").GetComponent<midPlayer>().GenerateUrl(getType);
@@ -257,7 +264,12 @@ public class MatrixCreate : MonoBehaviour {
 				int curScore = GameObject.Find ("FontController").GetComponent<FontController> ().getScore ();
 				PlayerPrefs.SetInt ("Score", curScore);
 				PlayerPrefs.SetString ("UserMatrix", Contectedmatrix);
-              
+                string gotTiles = "";
+                for (int i = 0; i < typeArray.Length; i++) {
+                    gotTiles += typeArray[i].ToString()+"|";
+                }
+                PlayerPrefs.SetString("resultTiles", gotTiles);
+                Debug.Log(gotTiles);
                 Invoke("EndSceneCall", 5.0f);
 				//SceneManager.LoadScene("endscene");
 			}
---
+++ Assets/SetResultTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetResultTile : MonoBehaviour
+{
+    private string result;
+    public Sprite[] BlackSprite;
+
+    void Start()
+    {
+        result = PlayerPrefs.GetString("resultTiles");
+        string[] tiles = result.Split('|');        
+        for (int i = 1; i < tiles.Length-1; i++) {
+            string typeName = "BottomTile" + i.ToString();
+            GameObject.Find(typeName).GetComponent<Image>().sprite = BlackSprite[Convert.ToInt32(tiles[i])];
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
