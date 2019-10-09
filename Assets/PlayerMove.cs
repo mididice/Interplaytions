9780b2a07933ba552cf3519c21c59e2d8ecfcc11
ho9science <full17@naver.com>
2019-10-10 02:29:15 +0900

    add count and play effetc function

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -13,8 +13,9 @@ struct Point
 public class PlayerMove : MonoBehaviour {
 
 	public GameObject curCube;
-	int curxPos, curyPos,maxPosX,maxPosY;
-	int CubeType,backPosx,backPosy;
+	int curxPos, curyPos, maxPosX, maxPosY;
+	int CubeType, backPosx, backPosy;
+    int visitTrue;
     //private bool contected;
 	Stack<Point> st;
 	// Use this for initialization
@@ -24,23 +25,26 @@ public class PlayerMove : MonoBehaviour {
 		curyPos = 0;
 		backPosx = 0;
 		backPosy = 0;
+        visitTrue = 0;
 		maxPosX = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getX();
 		maxPosY = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getY();
 	}
   
 
     public bool backPosEqual(){
-		KeyValuePair<int,int> tmp = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().curPosPeek ();
-		if (tmp.Key == -1)
-			return false;
-		else if (backPosx == tmp.Key && backPosy == tmp.Value)
-			return true;
+		KeyValuePair<int,int> lastSelected = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().curPosPeek ();
+        if (lastSelected.Key == -1)
+            return false;
+        else if (backPosx == lastSelected.Key && backPosy == lastSelected.Value) {
+            return true;
+        }
 		return false;
 	}
 
-
-	void Move(){
-		int curType = -1;
+    void Move(){
+        Vector3 movementUpCube = new Vector3(0, curCube.transform.localScale.y + 0.157f, 0);
+        Vector3 movementRightCube = new Vector3(curCube.transform.localScale.x + 0.157f, 0, 0);
+        int curType = -1;
 		int curGetType = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getGetType ();
 		if (Input.GetKeyUp (KeyCode.UpArrow)) {
 			if (curyPos + 1 < maxPosY) { 
@@ -48,7 +52,7 @@ public class PlayerMove : MonoBehaviour {
 				backPosy = curyPos;
 				curyPos += 1;
 				curType = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getPos (curxPos,curyPos);
-				int visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
+				visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
 				bool IsTrue = backPosEqual ();
 				if (((curType == 0 || curType == curGetType)&& visitTrue==0) || curGetType==-1||(IsTrue&& visitTrue==curGetType)) {
 					if ((IsTrue&& visitTrue==curGetType)) {
@@ -57,21 +61,21 @@ public class PlayerMove : MonoBehaviour {
                         }
                         eraseRoute(backPosx, backPosy);
                     }
-					GameObject.Find ("FontController").GetComponent<FontController> ().CountingTurn ();
-					transform.position += new Vector3 (0, (curCube.transform.localScale.y+0.157f), 0);
-                    GameObject.Find("effectSound").GetComponent<AudioSource>().Play();
+                    countTurnAndPlayEffect();
+                    transform.position += movementUpCube;
 				} else {
 					curyPos -= 1;
 				}
 				//st.Push (Point (curxPos, curyPos));
 			}
-		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+        }
+        else if (Input.GetKeyUp (KeyCode.DownArrow)) {
 			if (curyPos - 1 >= 0) {
 				backPosx = curxPos;
 				backPosy = curyPos;
 				curyPos -= 1;
 				curType = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getPos (curxPos,curyPos);
-				int visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
+				visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
 				bool IsTrue = backPosEqual ();
 				if (((curType == 0 || curType == curGetType)&& visitTrue==0) || curGetType==-1 || (IsTrue&& visitTrue==curGetType)) {
 					if ((IsTrue&& visitTrue==curGetType)) {
@@ -80,76 +84,76 @@ public class PlayerMove : MonoBehaviour {
                         }
                         eraseRoute(backPosx, backPosy);
                     }
-					GameObject.Find ("FontController").GetComponent<FontController> ().CountingTurn ();
-					transform.position -= new Vector3 (0, (curCube.transform.localScale.y+0.157f), 0);
-                    GameObject.Find("effectSound").GetComponent<AudioSource>().Play();
+                    countTurnAndPlayEffect();
+                    transform.position -= movementUpCube;
                 } else {
 					curyPos += 1;
 				}
 				//st.Push (Point (curxPos, curyPos));
 			}
-		} else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+        }
+        else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
 			if (curxPos - 1 >= 0) {
 				backPosx = curxPos;
 				backPosy = curyPos;
 				curxPos -= 1;
 				curType = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().getPos (curxPos,curyPos);
-				int visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
-				bool IsTrue = backPosEqual ();
-				if (((curType == 0 || curType == curGetType)&& visitTrue==0) || curGetType==-1 ||(IsTrue&& visitTrue==curGetType)) {
-					if ((IsTrue&& visitTrue==curGetType)) {
+				visitTrue = GameObject.Find ("Main Camera").GetComponent<MatrixCreate> ().visitChk (curxPos,curyPos);
+                bool IsTrue = backPosEqual();
+                if (((curType == 0 || curType == curGetType)&& visitTrue==0) || curGetType==-1 ||(IsTrue&& visitTrue==curGetType)) {                 
+                    if ((IsTrue&& visitTrue==curGetType)) {
                         if (curType == curGetType && curType == visitTrue){
                             stopAudio();
                         }
                         eraseRoute(backPosx, backPosy);
                     }
-					GameObject.Find ("FontController").GetComponent<FontController> ().CountingTurn ();
-					transform.position -= new Vector3 (curCube.transform.localScale.x+0.157f,  0, 0);
-                    GameObject.Find("effectSound").GetComponent<AudioSource>().Play();
+                    countTurnAndPlayEffect();
+                    transform.position -= movementRightCube;
                 } else {
 					curxPos += 1;
 				}
-
-				//st.Push (Point (curxPos, curyPos));
-			}
-		} else if (Input.GetKeyUp (KeyCode.RightArrow)) {
+                //st.Push (Point (curxPos, curyPos));
+            }
+        }
+        else if (Input.GetKeyUp (KeyCode.RightArrow)) {
 			if (curxPos + 1 < maxPosX) {
-                //addPos(curxPos, curyPos, curType, curGetType);
                 backPosx = curxPos;
                 backPosy = curyPos;
                 curxPos += 1;
                 curType = GameObject.Find("Main Camera").GetComponent<MatrixCreate>().getPos(curxPos, curyPos);
-                int visitTrue = GameObject.Find("Main Camera").GetComponent<MatrixCreate>().visitChk(curxPos, curyPos);
+                visitTrue = GameObject.Find("Main Camera").GetComponent<MatrixCreate>().visitChk(curxPos, curyPos);
                 bool IsTrue = backPosEqual();
-
-                if (((curType == 0 || curType == curGetType) && visitTrue == 0) || curGetType == -1 || (IsTrue && visitTrue == curGetType))
-                {
+                if (((curType == 0 || curType == curGetType) && visitTrue == 0) || curGetType == -1 || (IsTrue && visitTrue == curGetType)){
                     if ((IsTrue && visitTrue == curGetType)){
                         if (curType == curGetType && curType == visitTrue){
                             stopAudio();
                         }
                         eraseRoute(backPosx, backPosy);
                     }
-                    GameObject.Find("FontController").GetComponent<FontController>().CountingTurn();
-                    transform.position += new Vector3(curCube.transform.localScale.x + 0.157f, 0, 0);
-                    GameObject.Find("effectSound").GetComponent<AudioSource>().Play();
+                    countTurnAndPlayEffect();
+                    transform.position += movementRightCube;
                 }
-                else
-                {
-                    curxPos -= 1;
+                else{
+                    curyPos -= 1;
                 }
                 //st.Push (Point (curxPos, curyPos));
+                
             }
-		}
+            
+
+        }
         setXYAndType(curxPos, curyPos, curType, curGetType);
+    }
+
+    private void countTurnAndPlayEffect() {
+        GameObject.Find("FontController").GetComponent<FontController>().CountingTurn();        
+        GameObject.Find("effectSound").GetComponent<AudioSource>().Play();
     }
 
     private void setXYAndType(int curxPos, int curyPos, int curType, int curGetType) {
         GameObject.Find("Main Camera").GetComponent<MatrixCreate>().setXY(curxPos, curyPos);
-        if (curType > 0 && curType == curGetType)
-        {
+        if (curType > 0 && curType == curGetType ){
             GameObject.Find("Main Camera").GetComponent<MatrixCreate>().setGetType(-1);
-
         }
     }
 
